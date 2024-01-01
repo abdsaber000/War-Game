679b1a8943111b6acc48fee8ac43635501098cb4
abdsaber000 <abdsaber000@git.com>
2024-01-01 15:06:33 +0200

    initial commit

---
+++ War Game/Program.cs
@@ -0,0 +1,14 @@
+﻿using War_Game;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Warrior alice = new Warrior("Alice" , 1000 , 40 , 60);
+        Warrior bob = new Warrior("Bob", 1000, 30, 20);
+
+        Battle battle = new Battle(alice, bob);
+        battle.StartGame();
+
+    }
+}
